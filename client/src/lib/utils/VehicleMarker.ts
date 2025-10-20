cd271839d349d4b294880a75f04d3ee9a9fe3214
Lliam Symonds <lliamsymonds04@gmail.com>
2025-10-20 16:36:14 +1000

    moved vehicle marker to utils and added skeleton to mapview for marker

--- client/src/lib/utils/VehicleMarker.ts
+++ client/src/lib/utils/VehicleMarker.ts
@@ -11,7 +11,7 @@ function getVehicleIconSvg(vehicleType: string | null): string {
   return iconMap[vehicleType?.toLowerCase() || ''] || '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-question-icon lucide-question"><path d="M9 9a3 3 0 0 1 6 0c0 2-3 3-3 5"/><path d="M12 17h.01"/><circle cx="12" cy="12" r="10"/></svg>';
 }
 
-export function VehicleMarker(vehicle: VehicleInfo, size =40) {
+export function createVehicleMarker(vehicle: VehicleInfo, size = 40) {
   const iconSvg = getVehicleIconSvg(vehicle.vehicleType);
   const color = 'var(--color-primary)'
   return L.divIcon({
@@ -25,8 +25,7 @@ export function VehicleMarker(vehicle: VehicleInfo, size =40) {
         align-items: center; 
         justify-content: center; 
         box-shadow: 0 0 5px rgba(0,0,0,0.3); 
-      "
-      >
+      ">
         ${iconSvg}
       </div>
     `,
